cc50673f8766fa0fff041e68fc5ad90caa8f0eb3
icoretchi <iulian.coretchi@gmail.com>
2021-06-06 14:27:19 +0300

    Add Admin Panel Application

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,13 +1,30 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component'
-import { RouterModule } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component'
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+    ],
+  },
+];
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
-  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabled' })],
+  declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
