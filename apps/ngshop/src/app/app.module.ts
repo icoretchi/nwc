90771bc27e98c8d055b8e440852943b989c94435
icoretchi <iulian.coretchi@gmail.com>
2021-06-06 11:58:02 +0300

    Add base style and configuration

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,11 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { UiModule } from '@nwc/ui';
+import { AccordionModule } from 'primeng/accordion';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'products',
+    component: ProductListComponent,
+  },
+];
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    ProductListComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    UiModule,
+    AccordionModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  projects: ['<rootDir>/apps/ngshop'],
-};
+  projects: [
+    '<rootDir>/apps/ngshop',
+    '<rootDir>/apps/admin',
+    '<rootDir>/libs/ui',
+    '<rootDir>/libs/products',
+    '<rootDir>/libs/users',
+    '<rootDir>/libs/orders',
+  ],
+}
